47207e165e67f4088f7dddbf115913c77c86c3b9
AIS3N <demirel.sc@gmail.com>
2018-10-18 16:46:31 +0200

    Update my_putchar.c

--- lib/my_putchar.c
+++ lib/my_putchar.c
@@ -1,12 +1,6 @@
-/*
-** my_putchar.c for my_putchar.c in /home/demi/smajov_d
-** 
-** Made by SMAJOVIC Demirel
-** Login   <smajov_d@etna-alternance.net>
-** 
-** Started on  Fri Sep 23 18:04:47 2016 SMAJOVIC Demirel
-** Last update Sun Oct  2 18:23:34 2016 SMAJOVIC Demirel
-*/
+##
+## Created By AIS3N
+##
 
 #include <unistd.h>
 
