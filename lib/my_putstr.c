2489af3a1cc15560765a164e130d2186b49b30e1
AIS3N <demirel.sc@gmail.com>
2018-10-18 16:46:44 +0200

    Update my_putstr.c

--- lib/my_putstr.c
+++ lib/my_putstr.c
@@ -1,12 +1,6 @@
-/*
-** my_putstr.c for my_putstr.c in /home/demi/smajov_d/Jour03/smajov_d/my_putstr
-** 
-** Made by SMAJOVIC Demirel
-** Login   <smajov_d@etna-alternance.net>
-** 
-** Started on  Sun Sep 25 03:35:53 2016 SMAJOVIC Demirel
-** Last update Sun Sep 25 03:36:13 2016 SMAJOVIC Demirel
-*/
+##
+## Created By AIS3N
+##
 
 void	my_putchar(char c);
 
